cf3769d743563cc2ec186b412a487e700bbd6012
mary-khachatryan <mary.khachatryan2@gmail.com>
2024-08-19 00:11:18 +0400

    did some minor changes, added area plot

--- Armstat.py
+++ Armstat.py
@@ -1,20 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-
+import streamlit as st
 id  = '11006'
 url = f"https://armstat.am/am/?nid=12&id={id}&submit=%D5%93%D5%B6%D5%BF%D6%80%D5%A5%D5%AC"
 html = requests.get(url)
 soup = BeautifulSoup(html.content, "html.parser")
 id_dict = {}
 data_dict ={}
+labels = []
 
 
 
 sec_html_tags = soup.find_all('option')
 for  section in sec_html_tags:
-    id_dict[section.attrs['label']] = section.attrs['value']
-    
+
+    id_dict[section.attrs['label']] = (section.attrs['value'])
+    labels.append(section.attrs['label'])
 print(id_dict)
 
 
@@ -40,8 +42,25 @@ def page_data_parser(id):
     df = pd.DataFrame(one_page_data, columns=['Year', 'Value', 'decrease']) 
     name = soup.find('h2').text
     data_dict[name] = df
+    df['Value'] = df['Value'].replace('-', 0)
+    df['Value'] = df['Value'].astype(float)
+    return df
 
+option = st.selectbox("Choose ",labels,index=None,placeholder="Select contact method...",
+)
 
+id = id_dict[option]
+# id = '08010'
+source = page_data_parser(id)
+
+# source = pd.DataFrame({
+#     'Year': pd.date_range(start='2020', periods=10, freq='Y'),
+#     'Value': [3, 5, 7, 6, 8, 10, 6, 5, 9, 11]
+# })
+st.area_chart(source, x="Year", y="Value", color='#ff00dd')
+st.write("You selected:", option)
+
+print(labels)
 
 # df2= pd.read_html(url)
 # print(df2, "ասդ")
